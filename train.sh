f12e4000504edc66933a7c93049bf1d0084dc7bf
brettin <brettin@.anl.gov>
2023-09-14 17:10:25 -0500

    new file

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+#########################################################################
+### THIS IS A TEMPLATE FILE. SUBSTITUTE #PATH# WITH THE MODEL EXECUTABLE.
+#########################################################################
+
+# arg 1 CUDA_VISIBLE_DEVICES
+# arg 2 CANDLE_DATA_DIR
+# arg 3 CANDLE_CONFIG
+
+echo $@
